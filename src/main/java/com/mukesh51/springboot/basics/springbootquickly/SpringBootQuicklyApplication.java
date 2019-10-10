acf117a65f370374654ce2a44bacf8b2f349a827
Mukesh G Chandnani <mukesh51@gmail.com>
2019-10-10 10:12:57 +0100

    Initial Commit

---
+++ src/main/java/com/mukesh51/springboot/basics/springbootquickly/SpringBootQuicklyApplication.java
@@ -0,0 +1,12 @@
+package com.mukesh51.springboot.basics.springbootquickly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuicklyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuicklyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mukesh51/springboot/basics/springbootquickly/SpringBootQuicklyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mukesh51.springboot.basics.springbootquickly;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootQuicklyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
